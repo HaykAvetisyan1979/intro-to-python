6bd8e3e94ee9749b101385def1d7a50fce455bce
HaykAvetisyan1979 <haykave@yahoo.com>
2019-10-20 12:44:43 +0400

    Practice 1

---
+++ Week1/Problem2.py
@@ -0,0 +1,11 @@
+
+course = "Intro to Python"
+student = "Hayk Avetisyan"
+nickname = "Haykave"
+grade = 100
+python_expert = True
+print("course: ", course)
+print("student: ", student)
+print("nickname: ", nickname)
+print("grade: ", grade)
+print("python_expert: ", python_expert)
---
+++ Week1/Problem4.py
@@ -0,0 +1,4 @@
+ab = 3
+ac = 4
+bc = (ab**2 + ac**2)**0.5
+print("The hypotnuse of the triangle ABC = ", bc)
